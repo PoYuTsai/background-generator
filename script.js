90e25428d3fbe1927ebaba49d9e1e07579744674
蔡柏裕 <bobotsai1204@caiboyude-MacBook-Pro.local>
2018-12-11 21:07:36 +0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,42 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+
+//The following code is ok, 
+//but we write new function to avoid DRY
+
+// color1.addEventListener("input", function() {
+// 	body.style.background = 
+// 		"linear-gradient(to right, " 
+// 		+ color1.value
+// 		+ ", " 
+// 		+ color2.value 
+// 		+ ")";
+// })
+
+// color2.addEventListener("input", function() {
+// 	body.style.background = 
+// 		"linear-gradient(to right, " 
+// 		+ color1.value
+// 		+ ", " 
+// 		+ color2.value 
+// 		+ ")";
+// })
+
+function setGradient() {
+	body.style.background =
+	"linear-gradient(to right, " 
+	+ color1.value
+	+ ", " 
+	+ color2.value 
+	+ ")"; 
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
